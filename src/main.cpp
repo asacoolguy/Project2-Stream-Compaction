3b53b7756e9c78bf083836b32b16fdc97d26f626
asacoolguy <asacoolguy@gmail.com>
2018-09-14 16:07:52 -0400

    finished part 2

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,7 @@
 #include <stream_compaction/thrust.h>
 #include "testing_helpers.hpp"
 
-const int SIZE = 1 << 8
-; // feel free to change the size of array
+const int SIZE = 1 << 12; // feel free to change the size of array
 const int NPOT = SIZE - 3; // Non-Power-Of-Two
 int *a = new int[SIZE];
 int *b = new int[SIZE];
@@ -22,7 +21,6 @@ int *c = new int[SIZE];
 
 int main(int argc, char* argv[]) {
     // Scan tests
-
     printf("\n");
     printf("****************\n");
     printf("** SCAN TESTS **\n");
@@ -52,11 +50,11 @@ int main(int argc, char* argv[]) {
     printDesc("naive scan, power-of-two");
     StreamCompaction::Naive::scan(SIZE, c, a);
     printElapsedTime(StreamCompaction::Naive::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    //printArray(SIZE, c, true);
+    printArray(SIZE, c, true);
     printCmpResult(SIZE, b, c);
 
-	/* For bug-finding only: Array of 1s to help find bugs in stream compaction or scan
-	onesArray(SIZE, c);
+	// For bug-finding only: Array of 1s to help find bugs in stream compaction or scan
+	/*onesArray(SIZE, c);
 	printDesc("1s array for finding bugs");
 	StreamCompaction::Naive::scan(SIZE, c, a);
 	printArray(SIZE, c, true); */
@@ -65,7 +63,7 @@ int main(int argc, char* argv[]) {
     printDesc("naive scan, non-power-of-two");
     StreamCompaction::Naive::scan(NPOT, c, a);
     printElapsedTime(StreamCompaction::Naive::timer().getGpuElapsedTimeForPreviousOperation(), "(CUDA Measured)");
-    //printArray(SIZE, c, true);
+    printArray(SIZE, c, true);
     printCmpResult(NPOT, b, c);
 
     zeroArray(SIZE, c);
--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -11,15 +11,85 @@ namespace StreamCompaction {
             static PerformanceTimer timer;
             return timer;
         }
-        // TODO: __global__
+		__global__ void kernNaiveScan(int n, int* input, int* output, int interval) {
+			int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+			if (index < n) {
+				if (index < interval) {
+					output[index] = input[index];
+				}
+				else if (index >= interval) {
+					output[index] = input[index - interval] + input[index];
+				}
+			}
+		}
+
+		__global__ void kernMakeExclusive(int n, int* input, int* output) {
+			int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+			if (index < n){
+				if (index > 0) {
+					output[index] = input[index - 1];
+				}
+				else {
+					output[index] = 0;
+				}
+			}
+		}
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
-            timer().startGpuTimer();
-            // TODO
+			if (n <= 0) return;
+
+			// allocate 2 arrays on global memory
+			int* dev_buffer1;
+			int* dev_buffer2;
+			size_t sizeInBytes = n * sizeof(int);
+
+			cudaMalloc((void**)&dev_buffer1, sizeInBytes);
+			checkCUDAError("cudaMalloc dev_buffer1 failed!");
+
+			cudaMalloc((void**)&dev_buffer2, sizeInBytes);
+			checkCUDAError("cudaMalloc dev_buffer2 failed!");
+			
+			// copy the data into global memory
+			cudaMemcpy(dev_buffer1, idata, sizeInBytes, cudaMemcpyHostToDevice);
+			checkCUDAError("cudaMemcopy from idata to dev_buffer1 failed!");
+			cudaMemcpy(dev_buffer2, dev_buffer1, sizeInBytes, cudaMemcpyDeviceToDevice);
+			checkCUDAError("cudaMemcopy from dev_buffer1 to dev_buffer2 failed!");
+			
+			int blockSize = 256;
+			dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize);
+
+			timer().startGpuTimer();
+            
+			int ceiling = ilog2ceil(n);
+			int** input = &dev_buffer1;
+			int** output = &dev_buffer2;
+
+			for (int i = 1; i <= ceiling; i++) {
+				int interval = 1 << (i - 1);
+				kernNaiveScan << < fullBlocksPerGrid, blockSize >> > (n, *input, *output, interval);
+				checkCUDAError("kernNaiveScan failed");
+				std::swap(input, output);
+
+			}
+
+			// shift the output array to the right by 1
+			kernMakeExclusive << < fullBlocksPerGrid, blockSize >> > (n, *input, *output);
+			checkCUDAError("kernMakeExclusive failed");
+
             timer().endGpuTimer();
+
+			// copy data back 
+			cudaMemcpy(odata, *output, sizeInBytes, cudaMemcpyDeviceToHost);
+			checkCUDAError("cudaMemcopy from output to odata failed!");
+
+			// free the allocated arrays
+			cudaFree(dev_buffer1);
+			checkCUDAError("cudaFree on dev_buffer1 failed");
+			cudaFree(dev_buffer2);
+			checkCUDAError("cudaFree on dev_buffer2 failed");
         }
     }
 }
